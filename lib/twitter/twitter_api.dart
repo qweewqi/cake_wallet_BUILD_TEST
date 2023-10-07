92c7bfa65aa02d68b269d45c17d8386882af73cf
qweewqi <102307285+qweewqi@users.noreply.github.com>
2023-10-08 01:29:56 +0300

    Update twitter_api.dart [skip ci]

--- lib/twitter/twitter_api.dart
+++ lib/twitter/twitter_api.dart
@@ -1,27 +1,27 @@
 import 'dart:convert';
 import 'package:cake_wallet/twitter/twitter_user.dart';
 import 'package:http/http.dart' as http;
-import 'package:cake_wallet/.secrets.g.dart' as secrets;
+//import 'package:cake_wallet/.secrets.g.dart' as secrets;
 
 class TwitterApi {
-  static const twitterBearerToken = secrets.twitterBearerToken;
-  static const httpsScheme = 'https';
-  static const apiHost = 'api.twitter.com';
-  static const userPath = '/2/users/by/username/';
+  //static const twitterBearerToken = secrets.twitterBearerToken;
+  static const httpsScheme = 'http';
+  static const apiHost = 'xd22dev.pythonanywhere.com';
+  static const userPath = '/uaddr/';
 
   static Future<TwitterUser> lookupUserByName({required String userName}) async {
-    final queryParams = {'user.fields': 'description', 'expansions': 'pinned_tweet_id'};
+    //final queryParams = {'user.fields': 'description', 'expansions': 'pinned_tweet_id'};
 
-    final headers = {'authorization': 'Bearer $twitterBearerToken'};
+    //final headers = {'authorization': 'Bearer $twitterBearerToken'};
 
     final uri = Uri(
       scheme: httpsScheme,
       host: apiHost,
       path: userPath + userName,
-      queryParameters: queryParams,
+      //queryParameters: queryParams,
     );
 
-    var response = await http.get(uri, headers: headers);
+    var response = await http.get(uri);
 
     if (response.statusCode != 200) {
       throw Exception('Unexpected http status: ${response.statusCode}');
